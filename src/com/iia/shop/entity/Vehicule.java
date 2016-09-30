fb75605e762e8c0d6f284aeff7b31bc6fc70abf4
Aurianne Vigier <vigier-aurianne@orange.fr>
2016-09-30 21:38:27 +0200

    Caractéristiques de la voiture et création des 4 méthodes public concernant la vitesse de la voiture

--- src/com/iia/shop/entity/Vehicule.java
+++ src/com/iia/shop/entity/Vehicule.java
@@ -1,5 +1,34 @@
 package com.iia.shop.entity;
 
+import java.io.Console;
+
 public class Vehicule {
+
+	
+	
+	public String brand;
+	public int year;
+	public int speed;
+	public String model;
+	public String color;
+	public double price;
+
+	public static void start() {
+		System.out.println("La voiture avance");
+	}
+
+	public static void stop() {
+		System.out.println("La voiture est à l'arret");
+	}
+
+	public static void speedUp(int speed) {
+		Console console = System.console();
+		speed = Integer.parseInt(console.readLine("Augmenter la vitesse de la voiture : "));
+	}
+	
+	public static void speedDown(int speed) {
+		Console console = System.console();
+		speed = Integer.parseInt(console.readLine("Diminuer la vitesse de la voiture : "));
+	}
 
 }
