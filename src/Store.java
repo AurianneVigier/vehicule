73d7bc1661e35dd75b7485ab60b0c2a005eba1af
Aurianne Vigier <vigier-aurianne@orange.fr>
2016-10-01 18:26:58 +0200

    Switch case et méthode create

--- src/Store.java
+++ src/Store.java
@@ -1,15 +1,17 @@
 import java.io.Console;
 
+import com.iia.shop.entity.Vehicule;
+
 public class Store {
 
 	public static void main(String[] args) {
-		// tableau de voitures
+		// tableau de vehicules
 
-		int Vehicule[];
-		Vehicule = new int[10];
-		
+		String Voiture[];
+		Voiture = new String[10];
+
 		int sortir = 1;
-		do{
+		do {
 			int choix;
 			System.out.println("Que souhaitez-vous faire ?");
 			System.out.println(" ");
@@ -19,16 +21,37 @@ public class Store {
 			System.out.println("4- Modifier un vehicule");
 			System.out.println("5- Supprimer un vehicule");
 			System.out.println(" ");
-			
+
 			Console console = System.console();
 			choix = Integer.parseInt(console.readLine("Entrez votre choix : "));
-						
+
+			switch (choix) {
+			case 1:
+				Vehicule.readAll();
+				break;
+			case 2:
+				Vehicule.readSingle();
+				break;
+			case 3:
+				Vehicule.create(Voiture);
+				break;
+			case 4:
+				Vehicule.update();
+				break;
+			case 5:
+				Vehicule.delete();
+				break;
+			default:
+				System.out.println("Vous etes sorti de l'application");
+				break;
+			}
+
 			System.out.println(" ");
-			System.out.println("Voulez-vous recommencer une opération ?");
+			System.out.println("Voulez-vous faire autre chose ?");
 			sortir = Integer.parseInt(console.readLine("0- Non | 1- Oui "));
 			System.out.println(" ");
 			System.out.println(" ");
-		}while (sortir != 0);
+		} while (sortir != 0);
 	}
 
 }
--- src/com/iia/shop/entity/Vehicule.java
+++ src/com/iia/shop/entity/Vehicule.java
@@ -4,14 +4,22 @@ import java.io.Console;
 
 public class Vehicule {
 
-	
-	
-	public String brand;
-	public int year;
-	public int speed;
-	public String model;
-	public String color;
-	public double price;
+	public static String brand;
+	public static int year;
+	public static int speed;
+	public static String model;
+	public static String color;
+	public static double price;
+
+	public Vehicule(String brand, int year, int speed, String model, String color, double price) {
+		super();
+		this.brand = brand;
+		this.year = year;
+		this.speed = speed;
+		this.model = model;
+		this.color = color;
+		this.price = price;
+	}
 
 	public static void start() {
 		System.out.println("La voiture avance");
@@ -25,15 +33,101 @@ public class Vehicule {
 		Console console = System.console();
 		speed = Integer.parseInt(console.readLine("Augmenter la vitesse de la voiture : "));
 	}
-	
+
 	public static void speedDown(int speed) {
 		Console console = System.console();
 		speed = Integer.parseInt(console.readLine("Diminuer la vitesse de la voiture : "));
 	}
-	
-	public static void read (int saisie){
+
+	public static void read(int saisie) {
 		Console console = System.console();
 		saisie = Integer.parseInt(console.readLine("Quelle voiture voulez-vous voir ?"));
+	}
+
+	public static void readAll() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void readSingle() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void create(String[] Voiture) {
+		Voiture = new String[10];
+		Console console = System.console();
+		System.out.println("Entrez la marque : ");
+		brand = console.readLine("");
+		System.out.println("Entrez l'annee : ");
+		year = Integer.parseInt(console.readLine(""));
+		System.out.println("Entrez la vitesse : ");
+		speed = Integer.parseInt(console.readLine(""));
+		System.out.println("Entrez le modele : ");
+		model = console.readLine("");
+		System.out.println("Entrez la couleur : ");
+		color = console.readLine("");
+		System.out.println("Entrez le prix : ");
+		price = Double.parseDouble(console.readLine(""));
+
+	}
+
+	public static void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void delete() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
 	}
 
 }
